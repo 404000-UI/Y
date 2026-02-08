446f448fab8057c454f8a6e9ebeddad9957be11d
404000-UI <minmagic0604@kakao.com>
2026-02-08 15:33:25 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:y/routes/home.dart';
+import 'package:y/routes/signup.dart';
+
+final bool isLogin = false;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(ProviderScope(child: Main()));
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: isLogin ? Home() : Signup(),
+    );
+  }
+}
---
+++ lib/routes/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Home extends ConsumerStatefulWidget {
+  const Home({super.key});
+
+  @override
+  ConsumerState createState() => _HomeState();
+}
+
+class _HomeState extends ConsumerState<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Y", style: TextStyle(fontFamily: "Oneline")),
+      ),
+      body: Container(),
+    );
+  }
+}
